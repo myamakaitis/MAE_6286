cc5627429969584b928a70676acc029fe969ceb4
myamakaitis <86685567+myamakaitis@users.noreply.github.com>
2022-09-18 11:45:35 -0400

    In-Class 9-20

--- lessons/01_phugoid/phugoid.py
+++ lessons/01_phugoid/phugoid.py
@@ -129,5 +129,8 @@ def plot_flight_path(zt, z0, theta0, N=1000):
     ax.set_xlabel(r'$x$')
     ax.set_ylabel(r'$z$')
     ax.plot(x, -z, linestyle='-', linewidth=2.0)
-    ax.axis('scaled', adjustable='box')
-    pyplot.show()
+    
+    
+#     ax.axis('scaled') #, adjustable='box')  
+#     ax.set_adjustable('box')
+    fig.show()
